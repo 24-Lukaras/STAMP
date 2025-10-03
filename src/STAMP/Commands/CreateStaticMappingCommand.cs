c8190d9bb37fa440631899bc527d2b14a5c43986
24-Lukaras <85632668+24-Lukaras@users.noreply.github.com>
2025-10-03 14:12:34 +0200

    feat: command registration

---
+++ src/STAMP/Commands/CreateStaticMappingCommand.cs
@@ -0,0 +1,43 @@
+﻿using STAMP.Lib;
+using System.IO;
+
+namespace STAMP.Commands
+{
+    [Command(PackageGuids.STAMPString, PackageIds.CreateStaticMappingCommand)]
+    internal sealed class CreateStaticMappingCommand : BaseCommand<CreateStaticMappingCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            try
+            {
+                var doc = await VS.Documents.GetActiveDocumentViewAsync();
+
+                if (!IsCSharpDocument(doc))
+                    throw new ArgumentException("File is not a CSharp document");
+
+                var solution = await VS.Solutions.GetCurrentSolutionAsync();
+                if (solution is null)
+                    throw new ArgumentNullException("Solution path is null");
+
+                var documentContent = doc.TextBuffer.CurrentSnapshot.GetText();
+                MapperBuilder builder = new MapperBuilder(
+                    content: documentContent,
+                    compilationProvider: new RecursiveCompilationProvider(solution.FullPath));
+
+                var result = await builder.Process();
+
+                using (var edit = doc.TextBuffer.CreateEdit())
+                {
+                    edit.Replace(0, doc.TextBuffer.CurrentSnapshot.Length, result);
+                    edit.Apply();
+                }
+            }
+            catch (Exception ex)
+            {
+                await VS.MessageBox.ShowErrorAsync(ex.Message);
+            }
+        }
+
+        private bool IsCSharpDocument(DocumentView document) => Path.GetExtension(document.FilePath).ToLowerInvariant() == ".cs";
+    }
+}
